cea5a368547738ebcbe4b5d91385847df5d582d3
ichdenkenicht <dix@goerlichs.de>
2023-03-10 15:29:06 +0100

    init

---
+++ post.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#cargo b --release
+cargo objcopy --release -- -O binary out.bin
+
+#sudo dfu-util -a0 -s 0x08000000  -D out.bin
+#dfu-util -a0 -s 0x08000000  -D out.bin
+
+st-flash --connect-under-reset write out.bin 0x8000000
+
---
+++ src/main.rs
@@ -0,0 +1,195 @@
+#![deny(unsafe_code)]
+#![no_main]
+#![no_std]
+#![allow(unused_imports)]
+
+use rtic::app;
+
+#[app(device = stm32f4xx_hal::pac, peripherals = true, dispatchers = [USART6, SPI4, SPI5])]
+mod app {
+    use core::fmt::Write;
+    use core::panic::PanicInfo;
+    use core::sync::atomic::{AtomicUsize, Ordering};
+    use rtic::export::CriticalSection;
+
+    use stm32f4xx_hal::gpio::Analog;
+    use stm32f4xx_hal::{
+        gpio::*,
+        i2c::{self, I2c, Mode as i2cMode},
+        pac,
+        pac::NVIC,
+        pac::{interrupt, Interrupt},
+        prelude::*,
+        rtc::{self, Rtc},
+        serial::{Config, Serial},
+        spi::{self, Mode as spiMode, Spi},
+        timer::{CounterHz, Delay, Event, Timer},
+        adc::{config::AdcConfig, config::SampleTime, Adc, config::*},
+    };
+    use systick_monotonic::*;
+
+    use shared_bus;
+    //ADS1115
+    use ads1x1x::{
+        channel, ic as ads1x1xIc, mode, Ads1x1x, ComparatorPolarity, ComparatorQueue,
+        DataRate16Bit, FullScaleRange, SlaveAddr as adsSlaveAddr,
+    };
+
+    enum state{
+        stopped,
+        started,
+    }
+
+    #[shared]
+    struct Shared {
+        s2: Serial<pac::USART2, (Pin<'A', 2, Alternate<7>>, Pin<'A', 3, Alternate<7>>)>,
+    }
+
+    #[local]
+    struct Local {
+        led: Pin<'C', 13, Output<PushPull>>,
+        pin_a0: Pin<'A', 0, Input>, //Button
+
+        pin_a6: Pin<'A', 6, Analog>, //Current Sense
+        pin_a7: Pin<'A', 7, Analog>, //Current Sense
+
+        tim3: CounterHz<pac::TIM3>,
+        adc: Adc<pac::ADC1>,
+    }
+
+    #[monotonic(binds = SysTick, default = true)]
+    type Tonic = Systick<10000>;
+
+    #[init]
+    fn init(mut ctx: init::Context) -> (Shared, Local, init::Monotonics) {
+
+        let mut sys_cfg = ctx.device.SYSCFG.constrain();
+        //RCC
+        let rcc = ctx.device.RCC.constrain();
+        let clocks = rcc
+            .cfgr
+            .use_hse(25.MHz())
+            .sysclk(100.MHz())
+            .pclk1(50.MHz()) //max 50 MHz
+            .pclk2(50.MHz()) //max 100 MHz
+            .freeze();
+
+        //GPIO SPLITS
+        let gpioa = ctx.device.GPIOA.split();
+        let gpiob = ctx.device.GPIOB.split();
+        let gpioc = ctx.device.GPIOC.split();
+
+        let led = gpioc.pc13.into_push_pull_output();
+
+        //UART2
+        //PA3 -> RX2
+        //PA2 -> TX2
+
+        let pin_a2 = gpioa.pa2.into_alternate();
+        let pin_a3 = gpioa.pa3.into_alternate();
+
+        let mut s2 = Serial::new(
+            ctx.device.USART2,
+            (pin_a2, pin_a3),
+            Config::default()
+                .baudrate(115200.bps())
+                .wordlength_8()
+                .parity_none(),
+            &clocks,
+        )
+        .unwrap();
+        writeln!(s2, "Init...").ok();
+
+        //TIMER3
+        // 16 bit Timer 3u
+        let mut tim3 = ctx.device.TIM3.counter_hz(&clocks);
+        tim3.listen(Event::Update);
+        
+
+        //TIMER4
+        // 16 bit Timer 4
+        let mut tim4 = ctx.device.TIM4.counter_hz(&clocks);
+        tim4.listen(Event::Update);
+
+        let pin_a6 = gpioa.pa6.into_analog(); //Voltage Sense
+        let pin_a7 = gpioa.pa7.into_analog(); //Current Sense
+
+        let mut adc = Adc::adc1(ctx.device.ADC1, true, AdcConfig::default());
+        
+        
+
+        //let sample = adc.convert(&pin_a7, SampleTime::Cycles_480);
+        
+        //writeln!(s2, "ADC output: {}", sample).ok();
+
+
+        //Internal RTC
+        //let mut irtc = Rtc::new(hal_p.RTC, &mut hal_p.PWR);
+        //let d
+        //irtc.set_datetime(date);
+        //let time = irtc.get_datetime();
+        //writeln!(s2, "{:#?}", time).unwrap();
+
+        tim3.start(1.Hz()).ok();
+
+
+        //Button Interrupt
+        let mut pin_a0 = gpioa.pa0.into_pull_up_input();
+        pin_a0.make_interrupt_source(&mut sys_cfg);
+        pin_a0.trigger_on_edge(&mut ctx.device.EXTI, Edge::Falling);
+        pin_a0.enable_interrupt(&mut ctx.device.EXTI);
+
+
+
+        let mono = Systick::new(ctx.core.SYST, 100_000_000);
+        
+        (Shared {s2}, Local {led, tim3, pin_a0, pin_a6, pin_a7, adc}, init::Monotonics(mono))
+    }
+
+    // Background task, runs whenever no other tasks are running
+    #[idle]
+    fn idle(_: idle::Context) -> ! {
+        loop {
+            continue;
+        }
+    }
+
+    //Timer3 Interrupt
+    #[task(binds = TIM3, local = [tim3, led, pin_a6, pin_a7, adc], shared = [s2], priority = 6)]
+    fn on_tim3(mut ctx: on_tim3::Context) {
+        ctx.local.tim3.clear_interrupt(Event::Update);
+        ctx.local.led.toggle();
+
+        let samplei = ctx.local.adc.convert(ctx.local.pin_a7, SampleTime::Cycles_480);
+
+        let v_acs: f32 = samplei as f32 * 3.3/4096.0;
+
+        let samplev = ctx.local.adc.convert(ctx.local.pin_a6, SampleTime::Cycles_480);
+
+        let mut v_vol: f32 = samplev as f32 * 3.3/4096.0;
+
+        v_vol = v_vol * 375.0/75.0;
+
+        ctx.shared.s2.lock(|s2|{
+            writeln!(s2, "V_ACS: {}", v_acs).ok();
+            writeln!(s2, "V_VOL: {}", v_vol).ok();
+        });
+    }
+
+    #[task(binds = EXTI0, local = [pin_a0], shared = [s2])]
+    fn on_button(mut ctx: on_button::Context){
+        ctx.local.pin_a0.clear_interrupt_pending_bit();
+
+        ctx.shared.s2.lock(|s2|{
+            writeln!(s2, "button interrupt").ok();
+        });
+
+    }
+
+
+    #[panic_handler]
+    fn panic(_info: &PanicInfo) -> ! {
+        loop {}
+    }
+
+}
