fad06caf254dec9960d2ab93f5a956b06a5d9744
ichdenkenicht <dix@goerlichs.de>
2023-09-01 14:06:53 +0200

    dcf77raw

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use rtic::app;
 mod app {
     use core::fmt::Write;
     use core::panic::PanicInfo;
-    use core::sync::atomic::{AtomicUsize, Ordering};
+    use core::sync::atomic::{AtomicUsize, Ordering, AtomicU32};
     use rtic::export::CriticalSection;
 
     use stm32f4xx_hal::gpio::Analog;
@@ -23,26 +23,17 @@ mod app {
         rtc::{self, Rtc},
         serial::{Config, Serial},
         spi::{self, Mode as spiMode, Spi},
-        timer::{CounterHz, Delay, Event, Timer},
+        timer::{CounterHz, Delay, Event, Timer, CounterUs},
         adc::{config::AdcConfig, config::SampleTime, Adc, config::*},
     };
     use systick_monotonic::*;
 
-    use shared_bus;
-    //ADS1115
-    use ads1x1x::{
-        channel, ic as ads1x1xIc, mode, Ads1x1x, ComparatorPolarity, ComparatorQueue,
-        DataRate16Bit, FullScaleRange, SlaveAddr as adsSlaveAddr,
-    };
-
-    enum state{
-        stopped,
-        started,
-    }
+    static T_START: AtomicU32 = AtomicU32::new(0);
 
     #[shared]
     struct Shared {
         s2: Serial<pac::USART2, (Pin<'A', 2, Alternate<7>>, Pin<'A', 3, Alternate<7>>)>,
+        
     }
 
     #[local]
@@ -50,15 +41,16 @@ mod app {
         led: Pin<'C', 13, Output<PushPull>>,
         pin_a0: Pin<'A', 0, Input>, //Button
 
-        pin_a6: Pin<'A', 6, Analog>, //Current Sense
-        pin_a7: Pin<'A', 7, Analog>, //Current Sense
+        pin_b0: Pin<'B', 0, Output>, //Button
+        pin_b1: Pin<'B', 1, Input>, //Button
 
         tim3: CounterHz<pac::TIM3>,
-        adc: Adc<pac::ADC1>,
+        tim2: CounterUs<pac::TIM2>,
+
     }
 
     #[monotonic(binds = SysTick, default = true)]
-    type Tonic = Systick<10000>;
+    type Tonic = Systick<1000>;
 
     #[init]
     fn init(mut ctx: init::Context) -> (Shared, Local, init::Monotonics) {
@@ -80,6 +72,10 @@ mod app {
         let gpioc = ctx.device.GPIOC.split();
 
         let led = gpioc.pc13.into_push_pull_output();
+
+
+        //dcf77 power -> needs to pulled to ground only
+        let pin_b0 = gpiob.pb0.into_push_pull_output(); 
 
         //UART2
         //PA3 -> RX2
@@ -108,18 +104,17 @@ mod app {
 
         //TIMER4
         // 16 bit Timer 4
-        let mut tim4 = ctx.device.TIM4.counter_hz(&clocks);
-        tim4.listen(Event::Update);
+        //let mut tim4 = ctx.device.TIM4.counter_hz(&clocks);
+        let mut tim2 = ctx.device.TIM2.counter_us(&clocks);
+        //tim4.listen(Event::Update);
+        let _ = tim2.start(10_000_000u32.micros());
 
-        let pin_a6 = gpioa.pa6.into_analog(); //Voltage Sense
-        let pin_a7 = gpioa.pa7.into_analog(); //Current Sense
-
-        let mut adc = Adc::adc1(ctx.device.ADC1, true, AdcConfig::default());
         
         
+        
 
+        //let mut adc = Adc::adc1(ctx.device.ADC1, true, AdcConfig::default());
         //let sample = adc.convert(&pin_a7, SampleTime::Cycles_480);
-        
         //writeln!(s2, "ADC output: {}", sample).ok();
 
 
@@ -140,10 +135,17 @@ mod app {
         pin_a0.enable_interrupt(&mut ctx.device.EXTI);
 
 
+        //dcf77 Interrupt
+        let mut pin_b1 = gpiob.pb1.into_input();
+        pin_b1.make_interrupt_source(&mut sys_cfg);
+        pin_b1.trigger_on_edge(&mut ctx.device.EXTI, Edge::RisingFalling);
+        pin_b1.enable_interrupt(&mut ctx.device.EXTI);
+
+
 
         let mono = Systick::new(ctx.core.SYST, 100_000_000);
         
-        (Shared {s2}, Local {led, tim3, pin_a0, pin_a6, pin_a7, adc}, init::Monotonics(mono))
+        (Shared {s2}, Local {led, tim3, tim2, pin_a0, pin_b0, pin_b1}, init::Monotonics(mono))
     }
 
     // Background task, runs whenever no other tasks are running
@@ -155,35 +157,71 @@ mod app {
     }
 
     //Timer3 Interrupt
-    #[task(binds = TIM3, local = [tim3, led, pin_a6, pin_a7, adc], shared = [s2], priority = 6)]
+    #[task(binds = TIM3, local = [tim3], shared = [s2], priority = 6)]
     fn on_tim3(mut ctx: on_tim3::Context) {
         ctx.local.tim3.clear_interrupt(Event::Update);
-        ctx.local.led.toggle();
-
-        let samplei = ctx.local.adc.convert(ctx.local.pin_a7, SampleTime::Cycles_480);
-
-        let v_acs: f32 = samplei as f32 * 3.3/4096.0;
-
-        let samplev = ctx.local.adc.convert(ctx.local.pin_a6, SampleTime::Cycles_480);
-
-        let mut v_vol: f32 = samplev as f32 * 3.3/4096.0;
-
-        v_vol = v_vol * 375.0/75.0;
+        //ctx.local.led.toggle();
 
         ctx.shared.s2.lock(|s2|{
-            writeln!(s2, "V_ACS: {}", v_acs).ok();
-            writeln!(s2, "V_VOL: {}", v_vol).ok();
+            writeln!(s2, "HeartBeat <3").ok();
         });
     }
 
-    #[task(binds = EXTI0, local = [pin_a0], shared = [s2])]
+
+    #[task(binds = EXTI0, local = [pin_a0, pin_b0, time: test = test { a: 0, b: 0 }], shared = [s2])]
     fn on_button(mut ctx: on_button::Context){
         ctx.local.pin_a0.clear_interrupt_pending_bit();
+
+        ctx.local.pin_b0.toggle();
 
         ctx.shared.s2.lock(|s2|{
             writeln!(s2, "button interrupt").ok();
         });
 
+    }
+
+    #[task(binds = EXTI1, local = [pin_b1, tim2, led, start: u32 = 0], shared = [s2])]
+    fn on_dcf77(mut ctx: on_dcf77::Context){
+        ctx.local.pin_b1.clear_interrupt_pending_bit();
+
+        if ctx.local.pin_b1.is_high() {
+
+            ctx.local.led.toggle();
+
+            let now = ctx.local.tim2.now().ticks();
+
+
+            //let now = ctx.local.tim2.now();
+            ctx.shared.s2.lock(|s2|{
+                writeln!(s2, "ms vergangen: {:?}", now - *ctx.local.start).ok();
+            });
+
+            *ctx.local.start = 0;
+
+            ctx.local.tim2.start(10_000_000u32.micros());
+            
+            
+        } else if ctx.local.pin_b1. is_low(){
+            let mut now = ctx.local.tim2.now().ticks();
+
+            //T_START.store(now, Ordering::SeqCst);
+
+            *ctx.local.start = now;
+            
+            ctx.local.led.toggle();
+           //ctx.local.tim4.start(1_0000000u32.micros()); 
+        }
+
+        /*
+        ctx.shared.s2.lock(|s2|{
+            writeln!(s2, "dcf77 interrupt").ok();
+        });
+        */
+    }
+
+    struct test {
+        a: u32,
+        b: u32,
     }
 
 
